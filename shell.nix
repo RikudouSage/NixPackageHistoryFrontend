c2a385332de18f2d3625e3b9f0b0d455c3833eeb
Dominik Chrastecky <dominik@chrastecky.cz>
2023-11-10 20:52:24 +0100

    Add sls deploy

--- shell.nix
+++ shell.nix
@@ -3,7 +3,10 @@ pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
       nodejs_18
       nodePackages."@angular/cli"
+      nodePackages.serverless
       yarn
+      awscli
+      jq
     ];
     shellHook = ''
       source <(ng completion script)
